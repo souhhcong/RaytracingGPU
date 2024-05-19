a316ef81a32b82fac611417ab99996d6a4dfe4ad
souhhcong <souhhcong@gmail.com>
2024-05-19 12:34:41 +0200

    compile on school machine

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 NVCC = /usr/local/cuda/bin/nvcc
 
 raytracer:
-	$(NVCC) raytracer.cu -o raytracer -O3 -arch=sm_60 -std=c++11 -I/usr/local/cuda/include
+	$(NVCC) raytracer.cu -o raytracer -O3 -arch=sm_86 -std=c++17 -I/usr/local/cuda/include
 
 clean:
 	rm -f raytracer
--- raytracer.cu
+++ raytracer.cu
@@ -2,13 +2,13 @@
 
 __global__ void hello_world_thread() {
     size_t index = blockIdx.x * blockDim.x + threadIdx.x;
-    printf("Hello world %llu\n", size_t);
+    printf("Hello world %llu\n", index);
 }
 
 void hello_world_gpu() {
     const size_t BLOCK_SIZE = 2;
     size_t GRID_SIZE = 2;    
-    hello_world_thread<<<BLOCKS_NUM, THREADS_PER_BLOCK>>>();
+    hello_world_thread<<<GRID_SIZE, BLOCK_SIZE>>>();
     cudaDeviceSynchronize();
 }
 
